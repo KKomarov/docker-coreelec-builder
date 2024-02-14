9a40bd946a9e59bc4ee4f50746c299fc0bd1f3c5
Konstantin Komarov <gerayee@gmail.com>
2024-02-14 21:41:29 +0300

    cpio

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ docker build -t coreelec-builder .
 
 docker run -it --mount type=bind,source=$(pwd),target=/home/docker coreelec-builder
 
-# PROJECT=Amlogic-ce DEVICES=Amlogic-ng ARCH=arm make image
+# PROJECT=Amlogic-ce DEVICE=Amlogic-ng ARCH=arm make image
 
